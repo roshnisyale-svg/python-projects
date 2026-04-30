507fe017d1144aad038a30c99fe6cbfbfc9b8f3c
Roshni singh <roshnisingh.yale@gmail.com>
2026-04-30 20:42:17 +0530

    Added age checker program

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 name = input("enter your name: ")
-print("hello,", name)
-print("this is a new change")
+age = int(input("enter your age: "))
+
+if age >= 18:
+    print("hello" , name, "_ you are an adult.")
+else:
+    print("hello" , name, "_you are under 18.")
